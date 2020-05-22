0856d0c08089bdb22ba4aa1e43d5ec59104b296d
laurens <miers132@gmail.com>
2020-05-22 14:08:20 +0200

    rectangles: add can_hold function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -39,6 +43,12 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         rect.area()
+    );
+
+    println!(
+        "The rectangle {:#?} can hold itself obviously: {}.",
+        rect,
+	rect.can_hold(&rect),
     );
 }
 
