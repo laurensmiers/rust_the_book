1dfa283017b974f067c27c4f561e5160cbce41c2
laurens <miers132@gmail.com>
2020-05-22 13:40:09 +0200

    Add example on using structs

---
+++ struct_example/src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct User {
+    email: String,
+    username: String,
+    active: bool,
+    sign_in_count: u32,
+}
+
+fn build_user(email: String, username: String) -> User {
+     User {
+         email,
+	 username,
+	 active: true,
+	 sign_in_count: 0,
+     }
+}
+
+fn main() {
+    let user = build_user(String::from("Heisenberg"), String::from("mail@mail.com"));
+    println!("Hello to the User: {:?}!", user);
+}
