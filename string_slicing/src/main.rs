6c3d7f50ce10839b7b7bbdbdb9173476249af8b4
laurens <miers132@gmail.com>
2020-05-22 13:25:47 +0200

    Add string slicing example

---
+++ string_slicing/src/main.rs
@@ -0,0 +1,18 @@
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+	    return &s[..i]
+	}
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let s = String::from("Hello world");
+
+    println!("String: {}", s);
+    println!("First word: {}", first_word(&s));
+}
